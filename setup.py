a49c78100844cfb9362969b72c259f535ec8831a
Nolan Cutler <cutlern@oregonstate.edu>
2026-08-27 15:49:36 -0700

    manually provide version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-# The release workflow sets PACKAGE_VERSION from the pushed git tag.
+# The release workflow sets PACKAGE_VERSION from the version it was run with.
 version = os.environ.get("PACKAGE_VERSION", "0.1.13")
 
 setup(
